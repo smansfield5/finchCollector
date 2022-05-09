9693efcdd24c73e6be4ddc8c3e96187ab089790d
smansfield5 <smansfield5@yahoo.com>
2022-05-09 16:08:48 -0600

    details page

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class Finch(models.Model):
+    name = models.CharField(max_length=100)
+    color = models.CharField(max_length=100)
+    location = models.CharField(max_length=100)
+    age = models.IntegerField()
--- main_app/views.py
+++ main_app/views.py
@@ -1,27 +1,19 @@
 from django.shortcuts import render
-
+from .models import Finch
 # Create your views here.
-from django.http import HttpResponse
 
-class Finch:
-    def __init__(self, name, age, fav_food, location):
-        self.name = name
-        self.age = age
-        self.fav_food = fav_food
-        self.location = location
 
-finches = [
-    Finch('Tweety', 85, 'pizza', 'Florida'),
-    Finch('Frank', 21, 'pasta', 'Colorado'),
-    Finch('Samantha', 10, 'alpo', 'Kansas'),
-    Finch('Chirpy', 15, 'hot dogs', 'Montana')
-]
 
 def home(request):
-    return HttpResponse('<h1>Finches</h1>')
+    return render(request, 'home.html')
 
 def about(request):
     return render(request, 'about.html')
 
 def finches_index(request):
+    finches = Finch.objects.all()
     return render(request, 'finches/index.html', { 'finches': finches })
+
+def finches_detail(request, finch_id):
+    finch = Finch.objects.get(id=finch_id)
+    return render(request, 'finches/detail.html', { 'finch': finch })
