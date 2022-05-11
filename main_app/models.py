86d3671b38ff45811961b6a43cec81c8e3113dc8
smansfield5 <smansfield5@yahoo.com>
2022-05-11 12:39:01 -0600

    toys button added

--- main_app/models.py
+++ main_app/models.py
@@ -9,6 +9,16 @@ MEALS = (
     ('D', 'Dinner'),
 )
 
+class Toy(models.Model):
+    name = models.CharField(max_length=75)
+    color = models.CharField(max_length=75)
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse('toys_detail', kwargs={'pk': self.id})
+
 class Finch(models.Model):
     name = models.CharField(max_length=100)
     color = models.CharField(max_length=100)
@@ -26,15 +36,12 @@ class Finch(models.Model):
 
 class Feeding(models.Model):
     date = models.DateField('feeding date')
-    meal = models.CharField(
-        max_length=1,
-        choices=MEALS,
-        default=MEALS[0][0]
-    )
+    meal = models.CharField(max_length=1, choices=MEALS, default=MEALS[0][0])
     finch = models.ForeignKey(Finch, on_delete=models.CASCADE)
+    
     def __str__(self):
         return f'{self.get_meal_display()} on {self.date}'
 
-class Meta:
-    ordering = ['-date']
+    class Meta:
+        ordering = ['-date']
 
--- main_app/views.py
+++ main_app/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
-from django.views.generic import CreateView, UpdateView, DeleteView
-from .models import Finch
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.views.generic import ListView, DetailView
+from .models import Finch, Toy
 from .forms import FeedingForm
 # Create your views here.
 
@@ -44,3 +45,21 @@ def add_feeding(request, finch_id):
         new_feeding.save()
     return redirect('detail', finch_id=finch_id)
 
+
+class ToyList(ListView):
+    model = Toy
+
+class ToyDetail(DetailView):
+    model = Toy
+
+class ToyCreate(CreateView):
+    model = Toy
+    fields = '__all__'
+
+class ToyUpdate(UpdateView):
+    model = Toy
+    fields = ['name', 'color']
+
+class ToyDelete(DeleteView):
+    model = Toy
+    success_url = '/toys/'
